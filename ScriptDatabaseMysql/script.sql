85255f02c3b925d85ffbcdabe46d654ddb089660
julioreisbaptista <gambr@hotmail.com>
2024-03-12 18:16:07 -0300

    Ajuste script base

--- ScriptDatabaseMysql/script.sql
+++ ScriptDatabaseMysql/script.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE tarefas;
+DROP database tarefasteste;
 
-USE tarefas;
+CREATE DATABASE tarefasteste;
 
+USE tarefasteste;
 
 DROP TABLE IF EXISTS `tarefas`;
-/
+
 CREATE TABLE `tarefas` (
   `id` int NOT NULL AUTO_INCREMENT,
   `descricao` text,
   `data` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `status` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ;
